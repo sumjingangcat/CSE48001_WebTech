86e948044c881bd21e5ba3b7b38e458e5a0791f4
sumjingangcat <oj47191@gmail.com>
2024-03-10 18:57:53 +0900

    [Add] CoreConcept functionality & props grouping

--- udemy_react_complete_introduction/section3/01-starting-project/src/App.jsx
+++ udemy_react_complete_introduction/section3/01-starting-project/src/App.jsx
@@ -1,3 +1,7 @@
+import reactImg from "./assets/react-core-concepts.png"
+import componentImg from "./assets/components.png"
+import { CORE_CONCEPTS } from "./data.js"
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -9,7 +13,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {descriptions} React concepts you will need for almost any app you are
@@ -19,12 +23,38 @@ function Header() {
   );
 }
 
+function CoreConcept({image, title, description}){
+  return(
+    <li>
+      <img src = {image} alt = {title}/>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id = "core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept
+          {...CORE_CONCEPTS[0]}
+          />
+          <CoreConcept
+          {...CORE_CONCEPTS[1]}
+          />
+          <CoreConcept
+          {...CORE_CONCEPTS[2]}
+          />
+          <CoreConcept
+          {...CORE_CONCEPTS[3]}
+          />
+        </ul>
+        </section>
       </main>
     </div>
   );
